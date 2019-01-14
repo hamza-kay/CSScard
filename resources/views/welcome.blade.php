4a903650ee4d5a42a4959b34674b188ffe39f38c
hamza-kay <hamzahk1991@gmail.com>
2019-01-14 21:26:00 +0000

    CSS Card

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSS for Backend Devs</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="/css/app.css">
+
+    <link rel="stylesheet" href="/css/noramlise.css">
+
+
+</head>
+
+<body>
+
+
+<div class="card">
+
+    {{-- left --}}
+    <div class="card-left">
+
+        <a href="#" class="card-skill-button">PHP</a>
+
+        <img src="https://laracasts.test/images/icons/books.svg">
+
+        <span>Intermediate Difficulty</span>
+
+
+
+
+    </div>
+
+
+
+    {{-- right --}}
+
+    <div class="card-right">
+
+        <h3 class="card-title"><a href="#">Testing Jargon</a> </h3>
+
+        <p class="card-excerpt"> Theres no two ways about it: blahblah Theres no two ways about it: blahblah Theres no two ways about it: blahblah
+            Theres no two ways about it: blahblah Theres no two ways about it: blahblah Theres no two ways about it: blahblah
+        </p>
+
+        <div class="card-meta">
+
+            <div>
+
+                <img src="https://laracasts.test/images/icons/books.svg">
+                5 Lessons
+
+            </div>
+
+            <div>
+
+                <img src="https://laracasts.test/images/icons/clock.svg">
+
+                53:10 min
+
+            </div>
+
+
+
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+
+</body>
+
+
+</html>
